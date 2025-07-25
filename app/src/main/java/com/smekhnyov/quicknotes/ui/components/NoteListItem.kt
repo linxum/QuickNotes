03219559d0f5a338a6f4b36cf3bed766f33e1e43
linxum <49167389+linxum@users.noreply.github.com>
2025-07-25 21:53:02 +0300

    Optimize imports

--- app/src/main/java/com/smekhnyov/quicknotes/ui/components/NoteListItem.kt
+++ app/src/main/java/com/smekhnyov/quicknotes/ui/components/NoteListItem.kt
@@ -3,14 +3,9 @@ package com.smekhnyov.quicknotes.ui.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smekhnyov.quicknotes.ui.theme.QuickNotesTheme
