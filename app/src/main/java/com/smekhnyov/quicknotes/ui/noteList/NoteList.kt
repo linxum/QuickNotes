d7f315a1dfc70a7eaf0403296880c2c8c7c506e5
linxum <49167389+linxum@users.noreply.github.com>
2025-07-26 22:07:33 +0300

    Added empty list message

--- app/src/main/java/com/smekhnyov/quicknotes/ui/noteList/NoteList.kt
+++ app/src/main/java/com/smekhnyov/quicknotes/ui/noteList/NoteList.kt
@@ -1,5 +1,7 @@
 package com.smekhnyov.quicknotes.ui.noteList
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,6 +17,8 @@ import com.smekhnyov.quicknotes.ui.theme.QuickNotesTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.smekhnyov.quicknotes.data.Note
 import com.smekhnyov.quicknotes.ui.components.NoteListItem
 
@@ -46,13 +50,24 @@ fun NoteList(
                 }
             }
         ) { innerPadding ->
-            LazyColumn(modifier = androidx.compose.ui.Modifier.padding(innerPadding)) {
-                items(notes.size) { index ->
-                    NoteListItem(
-                        title = notes[index].title,
-                        body = notes[index].body,
-                        onClick = { onNoteClick(notes[index].id) }
-                    )
+            if (notes.isEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = stringResource(R.string.empty_note_list))
+                }
+            } else {
+                LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                    items(notes.size) { index ->
+                        NoteListItem(
+                            title = notes[index].title,
+                            body = notes[index].body,
+                            onClick = { onNoteClick(notes[index].id) }
+                        )
+                    }
                 }
             }
         }
@@ -70,4 +85,14 @@ fun NoteListPreview() {
         notes,
         onNoteClick = { /* Handle note click */ },
         onAddNote = { /* Handle add note */ })
+}
+
+@Preview
+@Composable
+fun NoteListEmptyPreview() {
+    NoteList(
+        notes = emptyList(),
+        onNoteClick = { /* Handle note click */ },
+        onAddNote = { /* Handle add note */ }
+    )
 }
